dc5b517a5895f43f5d605181fac6dc79ba384d51
Harshraj35 <rajharsh3536@gmail.com>
2026-04-17 00:47:14 +0530

    Delete backend directory

--- backend/routes/gameRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { startMission, getDashboard } = require('../controllers/gameController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/start-mission').post(protect, startMission);
-router.route('/dashboard').get(protect, getDashboard);
-
-module.exports = router;
